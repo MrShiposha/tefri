937c9e458a129634d6387b55338478e159a4a1c2
Daniel Shiposha <mrshiposha@gmail.com>
2018-11-06 04:54:36 +0300

    Add wrap if not wrapped to template in OutputTuple in Pipeline

---
+++ include/tefri/Pipeline.h
@@ -0,0 +1,25 @@
+#ifndef TEFRI_PIPELINE_H
+#define TEFRI_PIPELINE_H
+
+#include "external/metaxxa/metaxxa.h"
+
+namespace tefri
+{
+    template <typename _OperatorsTuple>
+    class Pipeline
+    {
+    public:
+        using OperatorsTuple = _OperatorsTuple;
+        static constexpr size_t LENGTH = OperatorsTuple::size();
+
+        using InputTuple     = typename metaxxa::Function<typename OperatorsTuple::template Parameter<0>>::Arguments;
+        using OutputTuple    = typename metaxxa::Type
+                               <
+                                   typename metaxxa::Function<typename OperatorsTuple::template Parameter<LENGTH - 1>>::Result
+                               >::template WrapToTemplateIfNotWrapped<metaxxa::Tuple>;
+
+    private:
+    };
+}
+
+#endif // TEFRI_PIPELINE_H
