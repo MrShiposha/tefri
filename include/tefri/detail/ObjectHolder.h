644a4a585f4eb2eec467d1cfb03819007d77f74c
Daniel Shiposha <mrshiposha@gmail.com>
2018-11-14 21:45:58 +0300

    ObjectHolder holds either reference_wrapper or shred_ptr

--- include/tefri/detail/ObjectHolder.h
+++ include/tefri/detail/ObjectHolder.h
@@ -24,16 +24,41 @@ namespace tefri::detail
     using ObjectType = typename ObjectTypeImpl<T>::Type;
 
 
+    template <typename T, bool IS_REFERENCE_WRAPPER = metaxxa::Type<T>::template is_instantiation_of<std::reference_wrapper>()>
+    struct ObjectWrapperImpl 
+    {
+        using Type = T;
+    };
+
+    template <typename T>
+    struct ObjectWrapperImpl<T, false>
+    {
+        using Type = std::shared_ptr<T>;
+    };
+
+    template <typename T>
+    using ObjectWrapper = typename ObjectWrapperImpl<T>::Type;
+
+    template <typename T>
+    auto make_object_wrapper(std::add_const_t<std::add_lvalue_reference_t<T>> object)
+    {
+        if constexpr (metaxxa::Type<T>::template is_instantiation_of<std::reference_wrapper>())
+            return object;
+        else
+            return std::make_shared<T>(object);
+    }
+
+
     template <typename T>
     class ObjectHolder
     {
     public:
-        using Object = ObjectType<T>;
+        using Object        = ObjectType<T>;
+        using ObjectRef     = std::add_lvalue_reference_t<Object>;
+        using ObjectWrapper = ObjectWrapper<T>;
 
-        using ObjectRef = std::add_lvalue_reference_t<Object>;
-
-        ObjectHolder(T object)
-        : object(object)
+        ObjectHolder(T &object)
+        : wrapped_object(make_object_wrapper<T>(object))
         {}
 
         ObjectHolder(const ObjectHolder &) = default;
@@ -48,16 +73,22 @@ namespace tefri::detail
 
         Object get_object_copy()
         {
-            return object;
+            if constexpr(metaxxa::Type<ObjectWrapper>::template is_instantiation_of<std::reference_wrapper>())
+                return wrapped_object;
+            else
+                return *wrapped_object;
         }
 
         ObjectRef get_object_ref()
         {
-            return object;
+            if constexpr(metaxxa::Type<ObjectWrapper>::template is_instantiation_of<std::reference_wrapper>())            
+                return wrapped_object;
+            else
+                return *wrapped_object;
         }
 
     private:
-        T object;
+        ObjectWrapper wrapped_object;
     };
 }
 
