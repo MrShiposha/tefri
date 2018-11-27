164652be1f3ef6813f48078e72e0cfe939656456
Daniel Shiposha <shipdan@yandex.ru>
2018-11-27 12:26:00 +0300

    Return code is negative index+1 of test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,14 @@
+#include <vector>
+
 #include "TestTefri.h"
 #include "TestPipeline.h"
 #include "TestRingQueue.h"
 #include "TestTakeLast.h"
 #include "TestObjectHolder.h"
+#include "TestUnwrapObjectHolders.h"
 #include "TestPipelineBuilder.h"
 #include "TestCarryOperator.h"
 #include "TestFilter.h"
-
-#include <vector>
-
 
 int main(int argc, char **argv)
 {
@@ -18,14 +18,15 @@ int main(int argc, char **argv)
         std::make_shared<TestRingQueue>(),
         std::make_shared<TestTakeLast>(),
         std::make_shared<TestObjectHolder>(),
+        std::make_shared<TestUnwrapObjectHolders>(),
         std::make_shared<TestPipelineBuilder>(),
         std::make_shared<TestCarryOperator>(),
         std::make_shared<TestFilter>()
     };
 
-    for(auto &&test : tests)
-        if(!test->test())
-            return -1;
+    for(size_t i = 0, size = tests.size(); i < size; ++i)
+        if(!tests[i]->test())
+            return -(i + 1);
 
     std::cout << "All test succeeded" << std::endl;
 
