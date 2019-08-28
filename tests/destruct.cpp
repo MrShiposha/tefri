855b41ba991de8202ff2e1d2c297dbbb811e1076
Daniel Shiposha <shipdan@yandex.ru>
2019-08-28 11:02:10 +0300

    Refactor destruct operator

--- tests/destruct.cpp
+++ tests/destruct.cpp
@@ -2,11 +2,30 @@
 #include <string>
 #include <iostream>
 
+#include "tests.h"
+
 struct TestDestruct
 {
     int a;
     double b;
     char c;
+
+    template <std::size_t INDEX>
+    auto &get()
+    {
+        if constexpr (INDEX == 0)
+            return a;
+        else if constexpr (INDEX == 1)
+            return b;
+        else if constexpr (INDEX == 2)
+            return c;
+    }
+
+    template <std::size_t INDEX>
+    const auto &get() const
+    {
+        return const_cast<TestDestruct*>(this)->template get<INDEX>();
+    }
 
     bool operator==(const TestDestruct &rhs) const
     {
@@ -15,6 +34,9 @@ struct TestDestruct
             && c == rhs.c;
     }
 };
+
+struct TestEmptyStruct
+{};
 
 namespace std
 {
@@ -42,41 +64,56 @@ namespace std
     template <>
     class tuple_size<TestDestruct> : public std::integral_constant<std::size_t, 3> {};
 
-    template <std::size_t INDEX>
-    auto &get(TestDestruct &&obj)
-    {
-        if constexpr (INDEX == 0)
-            return obj.a;
-        else if constexpr (INDEX == 1)
-            return obj.b;
-        else if constexpr (INDEX == 2)
-            return obj.c;
-    }
+    template <>
+    class tuple_size<TestEmptyStruct> : public std::integral_constant<std::size_t, 0> {};
 }
-
-#include "tests.h"
 
 TEST_CASE("[tefri::destruct]")
 {
-    auto m = tefri::detail::monad<Seq<TestDestruct>, Seq<std::pair<std::size_t, std::string>>>()
-        >> destruct()
-        >> [](auto &&next, auto&&... args)
+    bool _0_called = false;
+    bool _2_called = false;
+    bool _3_called = false;
+
+    auto m = tefri::detail::monad
+    <
+        Seq<TestDestruct>, 
+        Seq<TestEmptyStruct>,
+        Seq<std::pair<std::size_t, std::string>>
+    >() >> destruct()
+        >> [&](auto &&next, auto&&... args)
         {
+            if constexpr (sizeof...(args) == 0)
+            {
+                _0_called = true;
+            }
             if constexpr (sizeof...(args) == 2)
+            {
                 REQUIRE
                 (
                     std::pair<std::size_t, std::string> { args.get_ref()... } ==
                     std::pair<std::size_t, std::string> { 11, "Message" }
                 );
+
+                _2_called = true;
+            }
             else if constexpr (sizeof...(args) == 3)
+            {
                 REQUIRE
                 (
                     TestDestruct { args.get_ref()... } ==
                     TestDestruct { 42, 3.14, 'c' }
                 );
+
+                _3_called = true;
+            }
             return next();
         };
 
     m(TestDestruct { 42, 3.14, 'c' });
     m(std::pair<std::size_t, std::string> { 11, "Message" });
+    m(TestEmptyStruct {});
+
+    REQUIRE(_0_called);
+    REQUIRE(_2_called);
+    REQUIRE(_3_called);
 }
